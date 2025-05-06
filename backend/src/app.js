e97af66d684e2b916b3961a7735df0169408c18b
unknown <tauqeernizami1@gmail.com>
2025-05-06 15:36:30 +0530

    back to sq 1

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,14 +7,10 @@ app.get("/", (req, res)=>{
     res.send("Hello World, Chando")
 })
 
-
-
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
 app.use('/ai', aiRoutes);
-app.use('/api/auth', require('../src/routes/auth.js'))
-
 
 module.exports = app
